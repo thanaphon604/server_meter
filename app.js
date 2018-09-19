c6d7b4c90734be401a9d8b5b9a41f958267e59e4
thanaphon604 <poper.604@gmail.com>
2018-09-20 01:49:48 +0700

    2st Adminsignin singup

--- app.js
+++ app.js
@@ -17,8 +17,18 @@ var app = express()
 
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.send('helloo')
+})
+app.post('/testPost', (req, res) => {
+    res.send('helloo')
 })
 
 app.post('/postAdmin', (req, res ) => {
@@ -30,6 +40,31 @@ app.post('/postAdmin', (req, res ) => {
         res.send(d)
     }, (e) => {
         res.status(400).send(e)
+    })
+    // res.send(req.body.username+'  '+req.body.password)
+})
+app.get('/getAdmin', (req, res) => {
+    Admin.find().then((doc) => {
+        res.send(doc)
+    }, (err) => {
+        res.status(404).send(err)
+    })
+})
+app.get('/getAdminn', (req, res) => {
+    let usernameInput = req.headers['username']
+    let passwordInput = req.headers['password']
+    //find หาusername password สำหรับ login
+    Admin.find({
+        username: usernameInput,
+        password: passwordInput
+    }).then((Admin) => {
+        if (Admin.length == 1) {
+            res.send(Admin[0])
+        } else if (Admin.length == 0) {
+            res.status(404).send('sory not found is user')
+        }
+    }, (err) => {
+        res.status(404).send(err)
     })
 })
 
@@ -44,6 +79,8 @@ app.post('/postBuilding', (req, res ) => {
         res.status(400).send(e)
     })
 })
+
+
 
 app.listen(3000, () => {
     console.log('listen on port 3000')
