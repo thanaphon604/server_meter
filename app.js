78755044d744dbe5b9c846d78a379d28b36bcdfe
thanaphon604 <poper.604@gmail.com>
2019-05-09 23:31:08 +0700

    save

--- app.js
+++ app.js
@@ -1233,7 +1233,7 @@ app.post('/postPrint', (req, res) => {
         .moveDown(0.1)
         doc.fontSize(15).text(`ค่าเช่า                                                                                      ${stringData[i].rent}`)
         .moveDown(0.1)
-        doc.fontSize(15).text(`ค่าไฟฟ้า                   ${stringData[i].meter.beforusemeter}              ${stringData[i].meter.usemeter}           ${stringData[i].meter.usemetermonth}               ${stringData[i].pricemeter}                 ${stringData[i].meterTotal}`)
+        doc.fontSize(15).text(`ค่าไฟฟ้า                 ${stringData[i].meter.beforusemeter}             ${stringData[i].meter.usemeter}          ${stringData[i].meter.usemetermonth}               ${stringData[i].pricemeter}                 ${stringData[i].meterTotal}`)
         .moveDown(0.1)
         doc.fontSize(15).text(`ค่าน้ำ`)
 
