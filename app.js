f658cae138f6ca6aeae5bda7a49aeab8819729fd
thanaphon604 <poper.604@gmail.com>
2019-05-10 00:30:47 +0700

    save

--- app.js
+++ app.js
@@ -1242,7 +1242,10 @@ app.post('/postPrint', (req, res) => {
         .moveDown(1)
         doc.fontSize(24).text(`รวมทั้งสิ้น  ${stringData[i].rent +stringData[i].meterTotal+stringData[i].waterTotal }   บาท`)
         doc.underline(0, 350, 620, 45, { color: "black" })
+        doc.underline(0, 350, 620, 45, { color: "black" })
+        doc.underline(0, 350, 620, 45, { color: "black" })
         .moveDown(1)
+        //=============บน
         doc.fontSize(28).text('ใบเเจ้งหนี้(Invoice)', {
             align: 'center',
             fontSize: '30'
@@ -1253,7 +1256,7 @@ app.post('/postPrint', (req, res) => {
         doc.fontSize(24).text(`เลขที่ห้องพัก ${stringData[i].roomNumbermeter}`)
             .moveDown(0.1)
         doc.fontSize(24).text(`ประจำเดือนที่ ${stringData[i].datemeter}`)
-        doc.underline(50, 200, 500, 27, { color: "black" })
+        doc.underline(50, 400, 500, 27, { color: "black" })
 
             .moveDown(0.25)
         doc.fontSize(15).text(`           เลขมิเตอร์ก่อนหน้า เลขมิเตอร์ปัจจุบัน  จำนวนที่ใช้  ราคาต่อหน่วย/บาท  จำนวนเงินที่ต้องจ่าย`)
@@ -1263,7 +1266,7 @@ app.post('/postPrint', (req, res) => {
         doc.fontSize(15).text(`ค่าไฟฟ้า                ${stringData[i].meter.beforusemeter}              ${stringData[i].meter.usemeter}          ${stringData[i].meter.usemetermonth}                ${stringData[i].pricemeter}                  ${stringData[i].meterTotal}`)
         .moveDown(0.1)
         doc.fontSize(15).text(`ค่าน้ำ                   ${stringData[i].water.beforusewater}              ${stringData[i].water.usewater}          ${stringData[i].water.usewatermonth}                ${stringData[i].pricewater}                 ${stringData[i].waterTotal}`)
-        doc.underline(50, 300, 500, 27, { color: "black" })
+        doc.underline(50, 500, 500, 27, { color: "black" })
         //.text(JSON.stringify(page), 100, 100)
         .moveDown(1)
         doc.fontSize(24).text(`รวมทั้งสิ้น  ${stringData[i].rent +stringData[i].meterTotal+stringData[i].waterTotal }   บาท`)
