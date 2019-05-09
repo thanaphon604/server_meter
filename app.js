cd4831782cf6e3fe5202046518c6842df73c1cd2
thanaphon604 <poper.604@gmail.com>
2019-05-10 00:21:27 +0700

    save

--- app.js
+++ app.js
@@ -1241,7 +1241,7 @@ app.post('/postPrint', (req, res) => {
         //.text(JSON.stringify(page), 100, 100)
         .moveDown(1)
         doc.fontSize(24).text(`รวมทั้งสิ้น  ${stringData[i].rent +stringData[i].meterTotal+stringData[i].waterTotal }   บาท`)
-        doc.underline(0, 350, 600, 35, { color: "black" })
+        doc.underline(0, 350, 620, 45, { color: "black" })
         .moveDown(10)
         doc.fontSize(28).text('ใบเเจ้งหนี้(Invoice)', {
             align: 'center',
